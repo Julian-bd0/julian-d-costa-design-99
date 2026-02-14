88e204d5de413f26672bd3ba9424c2149c83b37a
Julian-bd0 <juliandcosta.bd@gmail.com>
2026-02-14 17:38:37 +0600

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/julian-d-costa-design-99/",   // 👈 তোমার repo নাম
+  base: "/julian-d-costa-design-99/",  
   plugins: [react()],
 });
